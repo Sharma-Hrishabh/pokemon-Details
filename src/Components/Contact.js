679a48728e099c96a3ab6c4409bf77fcd36cf703
Sharma-Hrishabh <30hrs1999@gmail.com>
2019-02-09 13:16:16 +0530

    posts route setup

---
+++ src/Components/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Contact = () => {
+
+  return (
+    <div className="container">
+      <h1 className="center">
+      Contact
+      </h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur auctor iaculis cursus. Cras faucibus orci a sem tempor rhoncus. Integer posuere massa non maximus feugiat. Vivamus sodales ut lacus vitae pellentesque. In non lacinia felis. Duis lacus libero, euismod vitae tempus nec, congue in sapien. Sed cursus massa metus. Suspendisse potenti. Donec euismod nisl vitae ipsum interdum laoreet. Sed viverra et sem sodales maximus.</p>
+    </div>
+
+  )
+
+
+}
+
+export default Contact;
