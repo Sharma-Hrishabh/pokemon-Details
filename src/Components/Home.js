ab5958609a33a1d0b56523406c2595a49eba822c
Sharma-Hrishabh <30hrs1999@gmail.com>
2019-02-10 11:31:07 +0530

    home connected with redux store

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,25 +1,27 @@
 import React,{ Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Pokeball from '../pokeball.png';
 class Home extends Component{
 
-state = {
-  posts:[ ]
-}
+// state = {
+//   posts:[ ]
+// }
 
-componentDidMount(){
-  // console.log('home');
-  axios.get('https://jsonplaceholder.typicode.com/posts').then((res)=>{
-    // console.log(res)
-    this.setState({
-      posts:res.data.slice(0,10)
-    })
-  })
-}
+// componentDidMount(){
+//   // console.log('home');
+//   axios.get('https://jsonplaceholder.typicode.com/posts').then((res)=>{
+//     // console.log(res)
+//     this.setState({
+//       posts:res.data.slice(0,10)
+//     })
+//   })
+// }
 
    render() {
-     const { posts } = this.state;
+     console.log(this.props)
+     const { posts } = this.props;
      const postList = posts.length ? (posts.map((post) => {
        return (
      <div className="post card" key={post.id}>
@@ -46,4 +48,9 @@ componentDidMount(){
 
 }
 
-export default Home;
+const mapStateToProps = (state)=>{
+  return {
+    posts:state.posts
+  }
+}
+export default connect(mapStateToProps)(Home);
